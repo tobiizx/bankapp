07514ae46c7900b7a52cc09d8853b41d96053235
Tobiasz Molenda <tobiasz.molenda2@teb.edu.pl>
2024-11-22 09:41:12 +0100

    dodanie nowego pliku i nowej metody do requestowania login

---
+++ upobank/LoginRequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace upobank
+{
+    internal class LoginRequest
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+
+        public LoginRequest(string login, string password)
+        {
+            this.login = login;
+            this.password = password;
+        }
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
